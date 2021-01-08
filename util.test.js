50260f6d449c9a9980b9cc4b5ce78d45ba7c8392
Michael <micjanic20@gmail.com>
2021-01-07 21:16:12 -0600

    second test

--- util.test.js
+++ util.test.js
@@ -4,3 +4,8 @@ test('should output name and age', () => {
   const text = generateText('Mike', 29);
   expect(text).toBe('Mike (29 years old)');
 })
+
+test('should output data-less text', () => {
+  const text = generateText("", null)
+  expect(text).toBe(' (null years old)')
+})
