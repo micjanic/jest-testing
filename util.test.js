0106e80ec79ab677a05c007193d8dc5ec5b71d1c
Michael <micjanic20@gmail.com>
2021-01-07 21:20:44 -0600

    dataless test

--- util.test.js
+++ util.test.js
@@ -3,9 +3,14 @@ const {generateText} = require('./util')
 test('should output name and age', () => {
   const text = generateText('Mike', 29);
   expect(text).toBe('Mike (29 years old)');
+  const text2 = generateText('Anna', 28);
+  expect(text2).toBe('Anna (28 years old)');
 })
 
 test('should output data-less text', () => {
   const text = generateText("", null)
   expect(text).toBe(' (null years old)')
+
+  const text2 = generateText();
+  expect(text2).toBe('undefined (undefined years old)')
 })
